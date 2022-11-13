e420efbbe51a44ebbba140c2fc40662088f4e362
Marceli Koćwin <988marceli@gmail.com>
2022-11-13 18:30:20 +0100

    Added info about the source of future data, and mockup img

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ function App() {
       <header className="App-header">
         <h1>Aqapi</h1>
       </header>
-      <main className="App-main"></main>
-      <footer className="App-footer"></footer>
+      <main className="App-main">
+        <img className="mapa" src="https://previews.123rf.com/images/ahasoft2000/ahasoft20001806/ahasoft2000180606197/102554267-hex-tile-poland-map-vector-geographic-scheme-in-light-blue-color-with-horizontal-gradient-abstract-p.jpg" alt="Mapa Polski"></img>
+      </main>
+      <footer className="App-footer">
+        <p>Dane na temat jakości powietrza w serwisie Aqapi zostały pozyskane ze strony <a href="http://powietrze.gios.gov.pl/pjp/home">GIOŚ</a></p>
+      </footer>
     </div>
   );
 }
