8902e2230428ae97f5dd9fc14010503e92284978
Marceli Koćwin <988marceli@gmail.com>
2022-11-26 00:44:36 +0100

    added a basic map with react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import React, {useState} from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet';
 import './App.css';
+import { Icon } from "leaflet";
+
+function MainMap(props) {
+  return (
+    <MapContainer center={[52.2, 19.2]} zoom={8} scrollWheelZoom={true}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+    </MapContainer>
+  );
+}
 
 function App() {
   return (
@@ -7,7 +21,8 @@ function App() {
         <h1>Aqapi</h1>
       </header>
       <main className="App-main">
-        <img className="mapa" src="https://previews.123rf.com/images/ahasoft2000/ahasoft20001806/ahasoft2000180606197/102554267-hex-tile-poland-map-vector-geographic-scheme-in-light-blue-color-with-horizontal-gradient-abstract-p.jpg" alt="Mapa Polski"></img>
+        <MainMap></MainMap>
+        {/* <img className="mapa" src="https://previews.123rf.com/images/ahasoft2000/ahasoft20001806/ahasoft2000180606197/102554267-hex-tile-poland-map-vector-geographic-scheme-in-light-blue-color-with-horizontal-gradient-abstract-p.jpg" alt="Mapa Polski"></img> */}
       </main>
       <footer className="App-footer">
         <p>Dane na temat jakości powietrza w serwisie Aqapi zostały pozyskane ze strony <a href="http://powietrze.gios.gov.pl/pjp/home">GIOŚ</a></p>
